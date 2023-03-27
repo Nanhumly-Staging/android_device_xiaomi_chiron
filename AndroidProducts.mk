877dab4381f58f2b865beafc621a2208cc33989e
Nanhumly <admin@warbler.cc>
2023-03-27 06:08:34 +0000

    chiron: exthm Bringup
    Change-Id: I2f297b98bfd2d5e995b4d601e87c059c324516a4
    Signed-off-by: Nanhumly <admin@warbler.cc>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_chiron.mk
+    $(LOCAL_DIR)/exthm_chiron.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_chiron-user \
-    lineage_chiron-userdebug \
-    lineage_chiron-eng
+    exthm_chiron-user \
+    exthm_chiron-userdebug \
+    exthm_chiron-eng
