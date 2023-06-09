d1df333708a810d9b521a27e7d34b60c59320f53
x0x00044 <zlewchan@outlook.com>
2023-06-09 16:45:16 +0200

    chiron: PixelExperience Bringup
    Change-Id: I2d5eb23454fbaf20546bf314c0f6cf30738c6603

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_chiron.mk
+    $(LOCAL_DIR)/aosp_chiron.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_chiron-user \
-    lineage_chiron-userdebug \
-    lineage_chiron-eng
+    aosp_chiron-user \
+    aosp_chiron-userdebug \
+    aosp_chiron-eng
