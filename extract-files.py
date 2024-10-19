9ff0a1df62f6e07778b1df678914bec60db32d97
LuK1337 <priv.luk@gmail.com>
2024-10-19 19:22:22 +0200

    chiron: Setup ELF checks
    Change-Id: I528a53be6ef3518bc04ee0a3115e076460064630

--- extract-files.py
+++ extract-files.py
@@ -18,6 +18,8 @@ from extract_utils.fixups_blob import (
 )
 
 namespace_imports = [
+    'hardware/qcom-caf/common/libqti-perfd-client',
+    'hardware/xiaomi',
     'vendor/xiaomi/msm8998-common',
 ]
 
@@ -35,7 +37,6 @@ module = ExtractUtilsModule(
     'chiron',
     'xiaomi',
     blob_fixups=blob_fixups,
-    check_elf=False,
     namespace_imports=namespace_imports,
 )
 
